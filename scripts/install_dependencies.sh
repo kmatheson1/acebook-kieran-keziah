afa113e803c886a7384ed2e3b10be9eaa409c4ff
Kieran Matheson <kieran.w.c.matheson@gmail.com>
2023-11-14 16:10:09 +0000

    re run

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,5 +2,4 @@
 
 sudo yum install https://rpm.nodesource.com/pub_18.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
 sudo yum install nodejs -y --setopt=nodesource-nodejs.module_hotfixes=1
-cd /home/ec2-user/
-npm install
+sudo npm install
