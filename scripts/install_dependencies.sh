98f1210e071fc16780c87b1de537edfc38576550
Kieran Matheson <kieran.w.c.matheson@gmail.com>
2023-11-14 15:57:39 +0000

    insatll_dependencies.sh update

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -s https://deb.nodesource.com/setup_18.x | sudo bash
-sudo apt install nodejs -y
+sudo yum install https://rpm.nodesource.com/pub_18.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y
+sudo yum install nodejs -y --setopt=nodesource-nodejs.module_hotfixes=1
 cd /home/ec2-user/
 npm install
