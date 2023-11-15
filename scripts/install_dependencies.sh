77bf2fd4e15e88353ef57b79cab990cd14dbdb8b
Kieran Matheson <kieran.w.c.matheson@gmail.com>
2023-11-15 17:42:04 +0000

    re run

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,12 +4,9 @@
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 
 # Set up NVM environment variables
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="/home/ec2-user/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
-# Source Bash Profile
-source ~/.bashrc
 
 # Install Node.js (version 18) and check for npm
 nvm install 18
@@ -23,7 +20,9 @@ else
     exit 1
 fi
 
+# Navigate to your app directory
 cd /var/acebook
+
 # Install application dependencies
 npm install
 
