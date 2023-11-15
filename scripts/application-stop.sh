6757bfa31be91ba2192fc33a876006647c161130
Keziah <keziah.qrsh@gmail.com>
2023-11-15 12:42:30 +0000

    edit app stop

--- scripts/application-stop.sh
+++ scripts/application-stop.sh
@@ -4,4 +4,12 @@ cd /var/acebook
 
 source /home/ec2-user/.bash_profile
 
-pm2 stop kk-acebook
+# Check if the application is running
+if pm2 describe kk-acebook &> /dev/null; then
+  # If running, stop and delete the application
+  pm2 stop kk-acebook
+  pm2 delete kk-acebook
+  echo "Application kk-acebook stopped."
+else
+  echo "Application kk-acebook is not running."
+fi
