6170f7eb47022af65ea8ea0057b9f6db06d9d469
Kieran Matheson <kieran.w.c.matheson@gmail.com>
2023-11-15 17:13:54 +0000

    re run

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
+# Install Node.js and npm
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
+sudo yum install -y nodejs npm
+
 # Use full paths to ensure compatibility
 NVM_DIR="$HOME/.nvm"
 export NVM_DIR
 
-# Install nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-
 # Load nvm
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
-
 
 cd /var/acebook
 # Use Node.js LTS version
