af11dbb41dcb4a021039e0c9d39be4ce371766af
Keziah <keziah.qrsh@gmail.com>
2023-11-15 12:47:25 +0000

    placeholder for appname

--- scripts/application-stop.sh
+++ scripts/application-stop.sh
@@ -4,12 +4,14 @@ cd /var/acebook
 
 source /home/ec2-user/.bash_profile
 
+APP_NAME="kk-acebook"
+
 # Check if the application is running
-if pm2 describe kk-acebook &> /dev/null; then
+if pm2 describe $APP_NAME &> /dev/null; then
   # If running, stop and delete the application
-  pm2 stop kk-acebook
-  pm2 delete kk-acebook
-  echo "Application kk-acebook stopped."
+  pm2 stop $APP_NAME
+  pm2 delete $APP_NAME
+  echo "Application $APP_NAME stopped."
 else
-  echo "Application kk-acebook is not running."
+  echo "Application $APP_NAME is not running."
 fi
