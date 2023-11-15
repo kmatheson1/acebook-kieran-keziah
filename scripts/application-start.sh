5a69d11249ec9ea59064102f424bb8fefac2326a
Kieran Matheson <kieran.w.c.matheson@gmail.com>
2023-11-15 15:53:49 +0000

    re run

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# Use full paths to ensure compatibility
+NVM_DIR="$HOME/.nvm"
+export NVM_DIR
+
+# Load nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# Navigate to your app directory
 cd /var/acebook
 
+# Start your application with pm2
 pm2 start app.js --name kk-acebook
