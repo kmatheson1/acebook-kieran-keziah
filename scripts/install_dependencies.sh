ffdc3412d9fe81836f710fb9851afa66ee056b4c
Kieran Matheson <kieran.w.c.matheson@gmail.com>
2023-11-15 09:44:55 +0000

    update apspec

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,8 +2,9 @@
 
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion  
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 source ~/.bashrc
 nvm install 18
 nvm use 18
+
