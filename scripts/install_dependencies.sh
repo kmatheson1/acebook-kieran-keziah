41f472bc1e4bb8d1f3dc3ca5eac81d09816852c0
Kieran Matheson <kieran.w.c.matheson@gmail.com>
2023-11-15 17:00:39 +0000

    re run

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -11,6 +11,8 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
+
+cd /var/acebook
 # Use Node.js LTS version
 nvm install 18
 nvm use 18
