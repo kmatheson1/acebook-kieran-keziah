3fe480657432fe116231b257825285217ef5c976
Kieran Matheson <kieran.w.c.matheson@gmail.com>
2023-11-15 17:35:32 +0000

    re run

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
-# Install Node.js and npm
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
-sudo yum install -y nodejs npm
+cd /var/acebook
 
-# Use full paths to ensure compatibility
-NVM_DIR="$HOME/.nvm"
-export NVM_DIR
+# Install NVM
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 
-# Load nvm
+# Set up NVM environment variables
+export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
-cd /var/acebook
-# Use Node.js LTS version
+# Source Bash Profile
+source ~/.bashrc
+
+# Install Node.js (version 18) and check for npm
 nvm install 18
 nvm use 18
 
-# Navigate to your app directory
-cd /var/acebook
+# Check if npm is installed
+if command -v npm &> /dev/null; then
+    echo "npm is installed."
+else
+    echo "npm is not installed. Something went wrong."
+    exit 1
+fi
 
+cd /var/acebook
 # Install application dependencies
 npm install
 
