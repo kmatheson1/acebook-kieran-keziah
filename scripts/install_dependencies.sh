dd76e3c60b5e3913917ef1592f34a835f5b8b2f1
Kieran Matheson <kieran.w.c.matheson@gmail.com>
2023-11-15 15:20:10 +0000

    re run

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+cd /var/acebook
+
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
@@ -9,7 +11,6 @@ source ~/.bashrc
 nvm install 18
 nvm use 18
 
-cd /var/acebook
 npm install
 
 npm install -g pm2
