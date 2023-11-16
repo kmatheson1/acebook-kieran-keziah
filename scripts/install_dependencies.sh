e9fc2abe4f73964da67719f8bebdc922b723cfdc
Kieran Matheson <kieran.w.c.matheson@gmail.com>
2023-11-16 16:45:12 +0000

    re run

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,6 +2,11 @@
 
 source /home/ec2-user/.bash_profile
 
+if [ ! -d "/var/acebook" ]; then
+    # If it doesn't exist, create it
+    mkdir /var/acebook
+fi
+
 # sudo yum remove nodejs
 curl -sL https://rpm.nodesource.com/setup_18.x | sudo -E bash -
 yum install -y nodejs
