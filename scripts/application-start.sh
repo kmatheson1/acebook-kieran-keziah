ef4729d81689ceed4eb4b898e4bc7d989a423a11
Kieran Matheson <kieran.w.c.matheson@gmail.com>
2023-11-16 11:32:46 +0000

    specify path for node

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Specify the Node.js version (replace with your desired version)
+NODE_VERSION="18"
+
+# Set the PATH to include the bin directory of the Node.js version
+export PATH="/usr/bin:$PATH"
+
 # Navigate to your app directory
 cd /var/acebook
 
