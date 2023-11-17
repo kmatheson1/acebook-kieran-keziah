350d362a7b02ffe156b99bbbd88bbbc26a8093f5
Kieran Matheson <kieran.w.c.matheson@gmail.com>
2023-11-17 09:56:51 +0000

    re run

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,33 +2,42 @@
 
 # source /home/ec2-user/.bash_profile
 
-# sudo yum remove nodejs
-curl -sL https://rpm.nodesource.com/setup_18.x | sudo bash -
-yum install -y nodejs
-# Install pm2 globally
-npm install pm2@latest -g
+# Check if Node.js is installed
+if ! command -v node &> /dev/null; then
+    # Install Node.js
+    curl -sL https://rpm.nodesource.com/setup_18.x | sudo bash -
+    yum install -y nodejs
+fi
 
-repo_file="/etc/yum.repos.d/mongodb-org-7.0.repo"
-repo_content="[mongodb-org-7.0]
+# Check if pm2 is installed
+if ! command -v pm2 &> /dev/null; then
+    # Install pm2 globally
+    npm install pm2@latest -g
+fi
+
+
+if ! command -v mongod &> /dev/null; then
+
+    # MongoDB repository file
+    repo_file="/etc/yum.repos.d/mongodb-org-4.4.repo"
+    repo_content="[mongodb-org-4.4]
 name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/amazon/2023/mongodb-org/7.0/x86_64/
+baseurl=https://repo.mongodb.org/yum/amazon/2023/mongodb-org/4.4/x86_64/
 gpgcheck=1
 enabled=1
-gpgkey=https://www.mongodb.org/static/pgp/server-7.0.asc
+gpgkey=https://www.mongodb.org/static/pgp/server-4.4.asc
 "
 
-# Check if MongoDB is already installed
-if ! command -v mongod &> /dev/null; then
     # Check if the repository file exists
     if [ ! -e "$repo_file" ]; then
-    # Create the repository file with the specified content
-    echo -e "$repo_content" | tee "$repo_file" > /dev/null
+        # Create the repository file with the specified content
+        echo -e "$repo_content" | tee "$repo_file" > /dev/null
     fi
 
     # Install MongoDB
     yum install -y mongodb-org
-fi
 
-# Start and enable MongoDB
-systemctl start mongod
-systemctl enable mongod
+    # Start and enable MongoDB
+    systemctl start mongod
+    systemctl enable mongod
+fi
