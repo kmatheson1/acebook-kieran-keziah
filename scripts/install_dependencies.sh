6fd621b8078d28e85fb99543083ac070ea447ebb
Kieran Matheson <kieran.w.c.matheson@gmail.com>
2023-11-17 10:30:42 +0000

    re run

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -20,12 +20,12 @@ if ! command -v mongod &> /dev/null; then
 
     repo_file="/etc/yum.repos.d/mongodb-org-7.0.repo"
     repo_content="[mongodb-org-7.0]
-    name=MongoDB Repository
-    baseurl=https://repo.mongodb.org/yum/amazon/2023/mongodb-org/7.0/x86_64/
-    gpgcheck=1
-    enabled=1
-    gpgkey=https://www.mongodb.org/static/pgp/server-7.0.asc
-    "
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/amazon/2023/mongodb-org/7.0/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-7.0.asc
+"
 
     # Check if the repository file exists
     if [ ! -e "$repo_file" ]; then
